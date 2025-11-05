2d7604e6c8215ec2ce1100ba4507429ef1d54e55
Praveen Jha <praveenjhafbg54321@gmail.com>
2025-11-05 18:56:08 +0530

    error laya

--- tfcode/provider.tf
+++ tfcode/provider.tf
@@ -9,6 +9,6 @@ terraform {
 provider "azurerm" {
   features {
   }
-  subscription_id                 = "6a2f2f71-0973-4850-8e0e-98ca8c0ed794"
+  subscription_id = "6a2f2f71-0973-4850-8e0e-98ca8c0ed794"
   resource_provider_registrations = "none"
 }
